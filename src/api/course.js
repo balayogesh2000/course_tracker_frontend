7d1e56bc2e8bf18a0647610370179fde68c21138
balayogesh <balayogesh2000@gmail.com>
2021-09-15 23:01:30 +0530

    Added production url

--- src/api/course.js
+++ src/api/course.js
@@ -1,32 +1,36 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api/v1/";
+let baseURL = "https://track-online-course.herokuapp.com/api/v1/course/";
+
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:5000/api/v1/course/";
+}
 
 export const createCourse = async (body) => {
-  await axios.post("http://localhost:5000/api/v1/course", body);
+  await axios.post(baseURL, body);
 };
 
 export const getAllCourses = async () => {
-  const data = await axios.get(baseURL + "course");
+  const data = await axios.get(baseURL);
   return data;
 };
 
 export const getCourse = async (id) => {
-  const data = await axios.get(baseURL + "course/" + id);
+  const data = await axios.get(baseURL + id);
   return data;
 };
 
 export const updateCourse = async (id, body) => {
-  const data = await axios.patch(baseURL + "course/" + id, body);
+  const data = await axios.patch(baseURL + id, body);
   return data;
 };
 
 export const deleteCourse = async (id) => {
-  const data = await axios.delete(baseURL + "course/" + id);
+  const data = await axios.delete(baseURL + id);
   return data;
 };
 
 export const getAllLearning = async (id) => {
-  const data = await axios.get(baseURL + "course/" + id + "/learning");
+  const data = await axios.get(baseURL + id + "/learning");
   return data;
 };
